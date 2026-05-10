e00c91bac3e179b360d6a9fadbe7655188d76601
Richard-Alves167 <riichardd.08@gmail.com>
2026-05-10 00:23:48 -0300

    FEAT: adiciona um arquivo JS ENUM de rotas iniciais das páginas do aplicativo

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const Routes = {
+        HOME: 'Home',
+        SIGNUP: 'SignUp',
+        LOGIN: 'Login',
+        PROFILE: 'Profile',
+        HISTORY: 'History',
+        WORLDCUP: 'WorldCup',
+        OLYMPIC: 'Olympic',
+        CHAMPIONSHIP: 'Championship',
+        SHOP: 'Shop',
+    }
+
+export default Routes;
