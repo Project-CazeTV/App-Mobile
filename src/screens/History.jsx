83864cc051357f33bec4250e46ddc0c24eaae0f2
Richard-Alves167 <riichardd.08@gmail.com>
2026-05-11 09:50:11 -0300

    FEAT: adiciona banner inicial de história

--- src/screens/History.jsx
+++ src/screens/History.jsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ColorTypes from '../enumsCategories/ColorTypes';
+import HeroBanner from '../features/History/HeroBanner';
 
 export default function History() {
   return (
     <View style={styles.container}>
-      <Text>History Screen!</Text>
+      <HeroBanner />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +17,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: ColorTypes.BACKGROUNDWHITE,
     alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
