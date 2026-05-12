38b3155418a9b3a0d2f71c60d7f9aaba8039e534
Richard-Alves167 <riichardd.08@gmail.com>
2026-05-12 20:46:31 -0300

    FEAT: adiciona componente de contagem regressiva e utiliza na tela de Copa do Mundo

---
+++ src/features/WorlCup/CountdownCard.jsx
@@ -0,0 +1,160 @@
+import { useCountdown } from '../../hooks/useCountdown';
+import logoFifa from '../../assets/images/logos/logo-copa.png';
+import logoCbf from '../../assets/images/logos/logo-cbf.png';
+import { StyleSheet, View, Image } from 'react-native';
+import AppText from '../../components/common/AppText';
+import ColorTypes from '../../enumsCategories/ColorTypes';
+import FontTypes from '../../enumsCategories/FontTypes';
+
+export default function CountdownCard() {
+    const timeLeft = useCountdown();
+
+    return (
+        <View style={styles.countdownWrapper}>
+            <View style={styles.DetailsBackground}>
+                <AppText style={styles.DetailBackground}>2026</AppText>
+                <AppText style={styles.DetailBackground2}>2026</AppText>
+            </View>
+            <View style={styles.content}>
+                <View style={styles.headerLogos}>
+                    <Image source={logoFifa} style={styles.logoFifa} />
+                    <View style={styles.divider} />
+                    <Image source={logoCbf} style={styles.logoCbf} />
+                </View>
+
+                <AppText style={styles.titleText}>COPA DO MUNDO É NA CAZÉ TV.</AppText>
+
+                <View style={styles.countdownGrid}>
+                    {Object.entries(timeLeft).map(([label, value]) => (
+                        <View key={label} style={styles.timeBlock}>
+                            <AppText style={styles.value}>{value}</AppText>
+                            <AppText style={styles.label}>{label}</AppText>
+                        </View>
+                    ))}
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    countdownWrapper: {
+        backgroundColor: ColorTypes.LIGHTBLUE,
+        zIndex: 1,
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+
+    bgDetails: {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+        opacity: 1,
+        pointerEvents: 'none',
+        zIndex: 1,
+    },
+
+        DetailsBackground: {
+        position: 'absolute',
+        overflow: 'hidden',
+        width: 1000,
+        height: 1000,
+        zIndex: 2,
+    },
+
+    DetailBackground: {
+        position: 'absolute',
+        fontFamily: FontTypes.COPA,
+        color: ColorTypes.GRAY,
+        opacity: 0.1,
+        zIndex: 1,
+        fontSize: 180,
+        top: -50,
+        left: 20,
+    },
+
+    DetailBackground2: {
+        position: 'absolute',
+        fontFamily: FontTypes.COPA,
+        color: ColorTypes.GRAY,
+        opacity: 0.1,
+        fontSize: 180,
+        zIndex: 1,
+        top: 50,
+        left: -190,
+        zIndex: 1,
+    },
+
+    content: {
+        position: 'relative',
+        zIndex: 2,
+        textAlign: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+        marginBottom: 20,
+    },
+
+    headerLogos: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        gap: '20px',
+        marginBottom: '25px',
+    },
+
+    logoFifa: {
+        height: '60px',
+        width: '60px',
+        resizeMode: 'contain',
+    },
+
+    logoCbf: {
+        height: '60px',
+        width: '60px',
+        resizeMode: 'contain',
+    },
+
+    divider: {
+        width: '1px',
+        height: '50px',
+        backgroundColor: ColorTypes.GRAY,
+        opacity: 0.3,
+    },
+
+    titleText: {
+        fontSize: '1.1rem',
+        fontWeight: 800,
+        textTransform: 'uppercase',
+        letterSpacing: '0.5px',
+        marginBottom: '50px',
+        lineHeight: '1.2',
+        color: ColorTypes.WHITE,
+    },
+
+    countdownGrid: {
+        flexDirection: 'row',
+        gap: '15px',
+        boxSizing: 'border-box',
+    },
+
+    timeBlock: {
+        alignItems: 'center',
+        gap: 20,
+    },
+
+    value: {
+        fontSize: '2rem',
+        fontWeight: 900,
+        lineHeight: 1,
+        color: ColorTypes.WHITE,
+    },
+
+    label: {
+        fontSize: '0.8rem',
+        fontWeight: 400,
+        textTransform: 'capitalize',
+        opacity: 0.7,
+        color: ColorTypes.WHITE,
+    },
+});
--- src/screens/WorldCup.jsx
+++ src/screens/WorldCup.jsx
@@ -1,21 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import AppText from '../components/common/AppText';
 import ColorTypes from '../enumsCategories/ColorTypes';
+import FontTypes from '../enumsCategories/FontTypes';
+import CountdownCard from '../features/WorlCup/CountdownCard';
+// import NextMatches from '../../features/WorldCup/components/NextMatches/NextMatches';
+// import FeaturedPlayers from '../../features/WorldCup/components/FeaturedPlayers/FeaturedPlayers';
+// import CopaSede from '../../features/WorldCup/components/CopaSede/CopaSede';
+
+// import NewsList from '../../components/Common/NewsList/NewsList';
+// import FilterSection from '../../components/Common/FilterSection/FilterSection';
+
+// import { useState } from 'react';
+// import { useLocation } from 'react-router-dom';
+// import { useSwipeable } from 'react-swipeable';
+
+// import { players } from '../../mocks/players';
+// import { newsWorldCup } from '../../mocks/newsWorldCup';
+// import { groups } from '../../mocks/groups';
+// import { octaves, quarters, semis, final } from '../../mocks/roundsWorldCup';
 
 export default function WorldCup() {
   return (
-    <View style={styles.container}>
-      <Text>World Cup Screen!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.mainContent}>
+        <CountdownCard />
+        {/* <CopaSede /> */}
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: ColorTypes.BACKGROUNDWHITE,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ pageContainer: {
+  backgroundColor: ColorTypes.BACKGROUNDWHITE,
+},
 });
