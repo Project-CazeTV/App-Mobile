1314fcc4995b9a4af8bd29ee7f4ae27a0ea2481c
Richard-Alves167 <riichardd.08@gmail.com>
2026-05-13 00:24:09 -0300

    FEAT: adiciona componente de notícias, cria um mock de notícias da FIFA26 e utiliza na tela de Copa do Mundo

---
+++ src/components/common/NewsList.jsx
@@ -0,0 +1,139 @@
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import Entypo from '@expo/vector-icons/Entypo';
+import { StyleSheet, View, Image, Pressable } from 'react-native';
+import AppText from '../common/AppText';
+import ColorTypes from '../../enumsCategories/ColorTypes';
+import FontTypes from '../../enumsCategories/FontTypes';
+
+export default function NewsList({ noticias = [], title, subtitle, onVerMais }) {
+    if (!noticias.length) {
+        return <AppText style={styles.empty}>Nenhuma notícia disponível.</AppText>;
+    }
+
+    return (
+        <View style={styles.listContainer}>
+            {title && <AppText style={styles.sectionTitle}>{title}</AppText>}
+            {subtitle && <AppText style={styles.sectionSubtitle}>{subtitle}</AppText>}
+
+            {noticias.map((noticia) => (
+                <View key={noticia.id} style={styles.newsCard}>
+                    <AppText style={[styles.tag, { backgroundColor: noticia.tagColor }]}>
+                            {noticia.categoria}
+                        </AppText>
+                    <View style={styles.imageWrapper}>
+                        <Image
+                            source={noticia.imagem}
+                            style={styles.image}
+                            onError={(e) => { e.target.src = '/placeholder-news.jpg'; }}
+                        />
+                    </View>
+                    <View style={styles.content}>
+                        <AppText style={styles.newsTitle}>{noticia.titulo}</AppText>
+                        {noticia.descricao && (
+                            <AppText numberOfLines={2} style={styles.newsDescription}>{noticia.descricao}</AppText>
+                        )}
+                        <View style={styles.footer}>
+                            <Pressable
+                                style={styles.verMais}
+                                onPress={() => onVerMais?.(noticia)}
+                            >
+                                Ver mais <Entypo name="arrow-with-circle-right" size={16} style={{ opacity: 0.8}} color={ColorTypes.DARK} />
+                            </Pressable>
+                            <AppText style={styles.tempo}>
+                                <FontAwesome5 name="clock" size={19} color={ColorTypes.GRAY}/> {noticia.tempo}
+                            </AppText>
+                        </View>
+                    </View>
+                </View>
+            ))}
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    listContainer: {
+        flexDirection: 'column',
+        gap: 25,
+        padding: 40,
+        width: '100%',
+    },
+
+    sectionTitle: {
+        fontWeight: '700',
+        fontSize: 20,
+    },
+
+    sectionSubtitle: {
+        fontSize: 15,
+        marginBottom: 20,
+        color: ColorTypes.GRAYTEXT,
+    },
+
+    newsCard: {
+        flexDirection: 'column',
+        gap: 15,
+        width: '100%',
+    },
+
+    imageWrapper: {
+        width: 220,
+    },
+
+    image: {
+        width: '100%',
+        aspectRatio: 16 / 9,
+        resizeMode: 'cover',
+        borderRadius: 4,
+    },
+
+    tag: {
+        alignSelf: 'flex-start',
+        paddingVertical: 4,
+        paddingHorizontal: 12,
+        fontSize: 12,
+        fontWeight: '900',
+        color: '#fff',
+        textTransform: 'uppercase',
+        marginBottom: 8,
+    },
+
+    newsTitle: {
+        fontSize: 16,
+        fontWeight: '900',
+        lineHeight: 21,
+        marginBottom: 10,
+    },
+
+    newsDescription: {
+        fontSize: 13,
+        color: ColorTypes.GRAYTEXT,
+        lineHeight: 20,
+        marginTop: 6,
+    },
+
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+
+    verMais: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 5,
+        opacity: 0.8,
+        fontSize: 13,
+        fontWeight: '600',
+        fontFamily: FontTypes.SORA,
+        color: ColorTypes.DARK,
+    },
+
+    tempo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        gap: 4,
+        opacity: 0.6,
+        fontSize: 16,
+        color: ColorTypes.GRAY,
+    },
+});
--- src/screens/WorldCup.jsx
+++ src/screens/WorldCup.jsx
@@ -6,24 +6,21 @@ import FontTypes from '../enumsCategories/FontTypes';
 import CountdownCard from '../features/WorlCup/CountdownCard';
 import CopaSede from '../features/WorlCup/CopaSede';
 import FeaturedPlayers from '../features/WorlCup/FeaturedPlayers';
+import NewsList from '../components/common/NewsList';
 import { sedes } from '../mocks/sedesWorldCup2026';
 import { players } from '../mocks/players';
+import { newsWorldCup } from '../mocks/newsWorldCup';
+
 // import NextMatches from '../../features/WorldCup/components/NextMatches/NextMatches';
-// import FeaturedPlayers from '../../features/WorldCup/components/FeaturedPlayers/FeaturedPlayers';
 
-// import NewsList from '../../components/Common/NewsList/NewsList';
-// import FilterSection from '../../components/Common/FilterSection/FilterSection';
-
-// import { useState } from 'react';
-// import { useLocation } from 'react-router-dom';
-// import { useSwipeable } from 'react-swipeable';
-
-// import { players } from '../../mocks/players';
-// import { newsWorldCup } from '../../mocks/newsWorldCup';
 // import { groups } from '../../mocks/groups';
 // import { octaves, quarters, semis, final } from '../../mocks/roundsWorldCup';
 
 export default function WorldCup() {
+  const handleVerMais = (noticia) => {
+    console.log("Ver mais:", noticia);
+  };
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.mainContent}>
@@ -31,6 +28,12 @@ export default function WorldCup() {
         <CopaSede sedes={sedes}/>
       </View>
       <FeaturedPlayers players={players} />
+            <NewsList
+        title="O que ta rolando por aí?"
+        subtitle={"Acompanhe os principais acontecimentos do futebol nacional e internacional."}
+        noticias={newsWorldCup}
+        onVerMais={handleVerMais}
+      />
     </ScrollView>
   );
 }
