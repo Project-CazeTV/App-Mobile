8d9ff5972451360d65d0e231cff0d93e6121bdc2
Richard-Alves167 <riichardd.08@gmail.com>
2026-05-26 11:41:04 -0300

    FEAT: adiciona card dos produtos do carrinho

--- src/screens/Cart.jsx
+++ src/screens/Cart.jsx
@@ -2,30 +2,25 @@ import { StyleSheet, View, ScrollView, Image } from 'react-native';
 import AppText from '../components/common/AppText';
 import ColorTypes from '../enumsCategories/ColorTypes';
 import HeaderStackReturnPage from '../routes/HeaderStackReturnPage';
-
-// import ProductSection from "../../features/Cart/Components/ProductsSection/ProductSection";
+import { productsMock } from "../mocks/products";
+import ProductSection from "../features/Shop/ProductSection";
 import CartPriceSection from "../features/Shop/CartPriceSection";
 
 export default function Cart() {
-    const itens = 10
+    const cart = productsMock
+    const itens = cart.length
     const subTitle = itens + (itens<=1 ? " Item" : " Itens")
     return (
         <>
             <HeaderStackReturnPage pageTitle={'Meu Carrinho'} pageSubTitle={subTitle} />
             <View style={styles.page}>
                 <View style={styles.content}>
-                    {/* <ProductSection
+                    <ProductSection
                         produtos={cart}
-                        onAumentar={aumentarQuantidade}
-                        onDiminuir={diminuirQuantidade}
-                        onRemover={removerProduto}
-                    /> */}
-                    {/* {cart.length > 0 && (
-                        <>
-                            <View style={styles.divider} />
-                            <PriceSection produtos={cart} />
-                        </>
-                    )} */}
+                        onAumentar={'aumentarQuantidade'}
+                        onDiminuir={'diminuirQuantidade'}
+                        onRemover={'removerProduto'}
+                    />
                 </View>
             </View>
             <CartPriceSection products={[]} />
@@ -39,7 +34,6 @@ const styles = StyleSheet.create({
     },
     content: {
         paddingTop: 20,
-        paddingBottom: 40,
         paddingHorizontal: 16,
         flex: 1,
     },
